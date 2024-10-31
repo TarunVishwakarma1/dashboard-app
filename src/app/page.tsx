b6136582666a52c77b29e0f7bc18e5e707c79849
Tarun Vishwakarma <138651451+TarunVishwakarma1@users.noreply.github.com>
2024-10-31 18:22:48 +0530

    Added dashboard with demo Data.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,8 @@
-import { ModeToggle } from "@/components/theme-provider/mode-toggle";
+export default  function Home() {
 
-export default function Home() {
   return (
-  <div>
-    <ModeToggle/>
-    <h1>Home Page</h1>
+      <div>
 
-  </div>  
+      </div>
   );
 }
